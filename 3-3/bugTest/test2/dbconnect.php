bcbcc2d6df670f8ec7a912d1a8ebd349b4a4f3fa
ramugisu <64357395+ramugisu@users.noreply.github.com>
2021-02-22 23:18:08 +0900

    課題提出　バグの修正

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "http://localhost/LetsEngineer/curriculumPHP/3-2/bugTest/test2/index.php";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "loginManagement";
+
+
+?>
